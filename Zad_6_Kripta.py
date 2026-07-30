046d236d9f8b09d0337a5e88bee46b6d87d6fbf9
Olesya <alexa7219@mail.ru>
2026-07-30 21:32:12 +0300

    Добавляем список криптовалют: ID монеты - её торговый символ

---
+++ Zad_6_Kripta.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime
+import requests
+
+coins = {
+    'bitcoin': 'BTC',
+    'ethereum': 'ETH',
+    'solana': 'SOL',
+    'cardano': 'ADA',
+    'ripple': 'XRP',
+    'dogecoin': 'DOGE'
+}
