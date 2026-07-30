7ef631ac67b734d242fcdd81b16a66ea19cd14a8
Olesya <alexa7219@mail.ru>
2026-07-30 21:46:39 +0300

    Создан графический интерфейс

--- Zad_6_Kripta.py
+++ Zad_6_Kripta.py
@@ -11,3 +11,24 @@ coins = {
     'ripple': 'XRP',
     'dogecoin': 'DOGE'
 }
+
+root = tk.Tk()
+root.title('Курсы криптовалют')
+root.geometry ('450x350')
+# Кнопка обновления
+btn = tk.Button(root, text='Обновить курсы')
+btn.pack(pady=10)
+# Время обновления
+time_lb = tk.Label(root, text='Обновлено: --')
+time_lb.pack()
+# таблица
+table = ttk.Treeview(root,columns=('name', 'symbol', 'price'), show='headings', height=8)
+table.heading('name', text='Криптовалюта')
+table.heading('symbol', text='Символ')
+table.heading('price', text='Цена (USD)')
+table.column('name', width=120)
+table.column('symbol', width=80)
+table.column('price', width=100)
+table.pack(pady=10, padx=10)
+
+root.mainloop()
